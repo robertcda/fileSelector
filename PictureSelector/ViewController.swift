1b8867bb598d380d8378c0301cf7cf0e4cef0b81
“robert” <robert.cda@in.ibm.com>
2016-08-04 15:43:58 +0530

    Initial Commit

---
+++ PictureSelector/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PictureSelector
+//
+//  Created by Robert on 04/08/16.
+//  Copyright © 2016 IBM. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
