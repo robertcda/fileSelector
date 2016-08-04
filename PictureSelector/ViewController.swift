5d511ae98e55dc5999c9e995cf426eb3eb319cc4
“robert” <robert.cda@in.ibm.com>
2016-08-04 15:59:13 +0530

    Adding a basic code to get started.

--- PictureSelector/ViewController.swift
+++ PictureSelector/ViewController.swift
@@ -10,18 +10,41 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var tableView: NSTableView!
+    
+    @IBOutlet weak var imageView: NSImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        tableView.setDataSource(self)
+        tableView.setDelegate(self)
+        
         // Do any additional setup after loading the view.
     }
 
+    @IBOutlet weak var nonSelectedCheckbox: NSButton!
+    @IBAction func nonSelectedClicked(sender: NSButton) {
+    }
+    
+    @IBOutlet weak var selectedCheckBox: NSButton!
+    
+    @IBAction func nonSelectedButtonClicked(sender: NSButton) {
+    }
+    
+    @IBAction func generateButtonClicked(sender: AnyObject) {
+    }
+    
     override var representedObject: AnyObject? {
         didSet {
         // Update the view, if already loaded.
         }
     }
-
-
 }
 
+extension ViewController: NSTableViewDataSource{
+    
+}
+
+extension ViewController: NSTableViewDelegate{
+    
+}
